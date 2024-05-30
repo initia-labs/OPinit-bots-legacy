757a92359181168b3e9d58b5d177199107fb63db
JSHan94 <saw151515@gmail.com>
2024-05-30 13:29:30 +0900

    fix resurrector

--- src/worker/bridgeExecutor/monitor/resurrector.ts
+++ src/worker/bridgeExecutor/monitor/resurrector.ts
@@ -1,6 +1,6 @@
 import { getDB } from '../db'
 import UnconfirmedTxEntity from '../../../orm/executor/UnconfirmedTxEntity'
-import { Coin, MsgFinalizeTokenDeposit } from 'initia-l2'
+import { Coin, Msg, MsgFinalizeTokenDeposit } from 'initia-l2'
 import { SECOND, config } from '../../../config'
 import { DataSource } from 'typeorm'
 import Bluebird from 'bluebird'
@@ -11,6 +11,8 @@ import {
   buildResolveErrorNotification,
   notifySlack
 } from '../../../lib/slack'
+
+const MAX_RESURRECT_SIZE = 100
 
 export class Resurrector {
   private db: DataSource
@@ -44,10 +46,7 @@ export class Resurrector {
     )
   }
 
-  async resubmitFailedDepositTx(
-    unconfirmedTx: UnconfirmedTxEntity
-  ): Promise<void> {
-    const txKey = `${unconfirmedTx.sender}-${unconfirmedTx.receiver}-${unconfirmedTx.amount}`
+  createMsg(unconfirmedTx: UnconfirmedTxEntity): Msg {
     const msg = new MsgFinalizeTokenDeposit(
       this.executorL2.key.accAddress,
       unconfirmedTx.sender,
@@ -58,13 +57,27 @@ export class Resurrector {
       unconfirmedTx.l1Denom,
       Buffer.from(unconfirmedTx.data, 'hex').toString('base64')
     )
+    return msg
+  }
+
+  createTxKey(unconfirmedTxs: UnconfirmedTxEntity[]): string {
+    return `${unconfirmedTxs[0].sender}-${unconfirmedTxs[0].receiver}-${unconfirmedTxs[0].amount}`
+  }
+
+  async resubmitFailedDepositTxs(
+    unconfirmedTxs: UnconfirmedTxEntity[]
+  ): Promise<void> {
+    const msgs = unconfirmedTxs.map((unconfirmedTx) =>
+      this.createMsg(unconfirmedTx)
+    )
+    const txKey = this.createTxKey(unconfirmedTxs)
     try {
-      await this.executorL2.transaction([msg])
-      await this.updateProcessed(unconfirmedTx)
+      await this.executorL2.transaction(msgs)
+      await Promise.all(unconfirmedTxs.map((tx) => this.updateProcessed(tx)))
       await notifySlack(
         txKey,
         buildResolveErrorNotification(
-          `[INFO] Transaction successfully resubmitted and processed for ${unconfirmedTx.sender} to ${unconfirmedTx.receiver} of amount ${unconfirmedTx.amount}.`
+          `[INFO] Transaction successfully resubmitted and processed from ${unconfirmedTxs[0].sequence} to ${unconfirmedTxs[unconfirmedTxs.length - 1].sequence} sequence.`
         ),
         false
       )
@@ -74,15 +87,15 @@ export class Resurrector {
         return
       }
       this.errorCounter = 0
-      await notifySlack(txKey, buildFailedTxNotification(unconfirmedTx))
+      await notifySlack(txKey, buildFailedTxNotification(unconfirmedTxs[0]))
       this.logger.error(
-        `[resubmitFailedDepositTx - ${this.name()}] Failed to resubmit tx: bridge id ${unconfirmedTx.bridgeId} sequence ${unconfirmedTx.sequence}`,
+        `[resubmitFailedDepositTxs - ${this.name()}] Failed to resubmit txs: bridge id ${unconfirmedTxs[0].bridgeId} sequence ${unconfirmedTxs[0].sequence}`,
         err
       )
     }
   }
 
-  async getunconfirmedTxs(): Promise<UnconfirmedTxEntity[]> {
+  async getUnconfirmedTxs(): Promise<UnconfirmedTxEntity[]> {
     return await this.db.getRepository(UnconfirmedTxEntity).find({
       where: {
         processed: false
@@ -91,7 +104,7 @@ export class Resurrector {
   }
 
   public async ressurect(): Promise<void> {
-    const unconfirmedTxs = await this.getunconfirmedTxs()
+    const unconfirmedTxs = await this.getUnconfirmedTxs()
 
     if (unconfirmedTxs.length === 0) {
       this.logger.info(`[ressurect - ${this.name()}] No unconfirmed txs found`)
@@ -101,14 +114,22 @@ export class Resurrector {
     this.logger.info(
       `[ressurect - ${this.name()}] Found ${unconfirmedTxs.length} unconfirmed txs`
     )
+
+    const unconfirmedTxsChunks: UnconfirmedTxEntity[] = []
     for (const unconfirmedTx of unconfirmedTxs) {
-      const error = unconfirmedTx.error
       // Check x/opchild/errors.go
-      if (error.includes('deposit already finalized')) {
+      if (unconfirmedTx.error.includes('deposit already finalized')) {
         await this.updateProcessed(unconfirmedTx)
         continue
       }
-      await this.resubmitFailedDepositTx(unconfirmedTx)
+      unconfirmedTxsChunks.push(unconfirmedTx)
+      if (unconfirmedTxsChunks.length === MAX_RESURRECT_SIZE) {
+        await this.resubmitFailedDepositTxs(unconfirmedTxsChunks)
+        unconfirmedTxsChunks.length = 0
+      }
+    }
+    if (unconfirmedTxsChunks.length > 0) {
+      await this.resubmitFailedDepositTxs(unconfirmedTxsChunks)
     }
   }
 }
