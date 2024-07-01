955e208fc89e7ed8ebd32fb73157916e27b97493
JSHan94 <saw151515@gmail.com>
2024-07-01 15:47:57 +0900

    configurable keep-alive option

--- src/lib/apiRequester.ts
+++ src/lib/apiRequester.ts
@@ -1,6 +1,7 @@
 import { APIRequester } from 'initia-l1'
 import http from 'http'
 import https from 'https'
+import { config } from 'config'
 
 class APIRequesterSingleton {
   private static instances: { [key: string]: APIRequester } = {}
@@ -10,8 +11,8 @@ class APIRequesterSingleton {
   public static getInstance(uri: string): APIRequester {
     if (!APIRequesterSingleton.instances[uri]) {
       APIRequesterSingleton.instances[uri] = new APIRequester(uri, {
-        httpAgent: new http.Agent({ keepAlive: true }),
-        httpsAgent: new https.Agent({ keepAlive: true })
+        httpAgent: new http.Agent({ keepAlive: config.ENABLE_KEEP_ALIVE }),
+        httpsAgent: new https.Agent({ keepAlive: config.ENABLE_KEEP_ALIVE })
       })
     }
     return APIRequesterSingleton.instances[uri]
